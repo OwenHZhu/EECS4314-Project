3f279cbe18f19241bf9a45d9a76d11b5a8282c93
Angelina De Santis <angelinadesantis03@gmail.com>
2026-06-18 13:10:42 -0400

    Updated the tailwind config with some global colours for styling.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#0C0707',
+        'primary-text': '#F9EDCC',
+        'secondary-text': '#8A3033',
+        'tertiary-text': '#BFB8AD',
+        'login-button': '#8A3033'
+      }
+    },
   },
   plugins: [],
 };
