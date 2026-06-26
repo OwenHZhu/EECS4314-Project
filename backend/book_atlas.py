844faed7c2000e7c06d70f614ddbedf167bcc2ba
Yawei Dong <ydong77@my.yorku.ca>
2026-06-25 22:54:40 -0400

    Register library router with API app

--- backend/book_atlas.py
+++ backend/book_atlas.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware # needed for connecting to React App Later
 
-from routers import auth
+from routers import auth, lib
 from utils.constants import ORIGINS
 
 app = FastAPI(title="BookAtlas API", description="Backend for BookAtlas reading platform", version="1.0.0")
 
 app.add_middleware(CORSMiddleware, allow_origins=ORIGINS, allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 app.include_router(auth.router, prefix="/api/v1")
+app.include_router(lib.router, prefix="/api/v1")
